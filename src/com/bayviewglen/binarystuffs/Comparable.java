52cae8fdce34ad403c1b4f0c81b2dda82d20a8ce
ChristopherAlexiev <ChristopherAlexiev@users.noreply.github.com>
2017-08-31 14:27:01 -0400

    Organized Files

--- src/com/bayviewglen/binarystuffs/Comparable.java
+++
@@ -1,7 +0,0 @@
-package com.bayviewglen.binarystuffs;
-
-public interface Comparable {
-	int compareTo(Comparable comparable);
-	String toStringForSave();
-	boolean contactContainsInfo(Comparable comparable);
-}
